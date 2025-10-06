a19862516c41b54d1784aaf4a9e257cbbeaaa6d7
Ahmad <ahmadhnazim@gmail.com>
2025-10-06 15:45:22 +0100

    Implement tests for isCloseTo

--- src/test/java/uk/ac/ed/acp/cw2/ServiceControllerTest.java
+++ src/test/java/uk/ac/ed/acp/cw2/ServiceControllerTest.java
@@ -67,4 +67,59 @@ public class ServiceControllerTest {
                         .content(invalidJson))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void testIsCloseTo_True() throws Exception {
+        // Points very close to each other
+        Position p1 = new Position();
+        p1.setLat(55.946233);
+        p1.setLng(-3.192473);
+
+        Position p2 = new Position();
+        p2.setLat(55.946300);
+        p2.setLng(-3.192400);
+
+        DistanceRequest request = new DistanceRequest();
+        request.setPosition1(p1);
+        request.setPosition2(p2);
+
+        mockMvc.perform(post("/api/v1/isCloseTo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    public void testIsCloseTo_False() throws Exception {
+        // Points further apart
+        Position p1 = new Position();
+        p1.setLat(55.946233);
+        p1.setLng(-3.192473);
+
+        Position p2 = new Position();
+        p2.setLat(55.942617);
+        p2.setLng(-3.192473);
+
+        DistanceRequest request = new DistanceRequest();
+        request.setPosition1(p1);
+        request.setPosition2(p2);
+
+        mockMvc.perform(post("/api/v1/isCloseTo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
+    }
+
+    @Test
+    public void testIsCloseTo_InvalidJson() throws Exception {
+        String invalidJson = "{ \"position1\": { \"lat\": 55.946233 } }"; // Missing fields
+
+        mockMvc.perform(post("/api/v1/isCloseTo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest());
+    }
+
 }
